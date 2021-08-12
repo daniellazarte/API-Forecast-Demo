6ac7bcf437428ec4edf962d2f7e5d660161eb45f
Daniel <daniel.lazarte@gmail.com>
2021-08-11 21:13:21 -0500

    Controlador de Saludo

---
+++ src/API/API Forecast 1.0/Controllers/saludoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Forecast_1._0.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class saludoController : ControllerBase
+    {
+        [HttpGet]
+        public string saludar() {
+            return "Hola Bienvenido al API";
+        
+        }
+            
+    }
+}
